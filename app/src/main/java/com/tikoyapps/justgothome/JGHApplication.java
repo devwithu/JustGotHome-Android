8ddff6357ee8510a4a4a4b2d4e41c2a7b291c9cb
Xyriz Tan <xcptan@gmail.com>
2016-04-27 20:50:31 +0800

    Initial commit

---
+++ app/src/main/java/com/tikoyapps/justgothome/JGHApplication.java
@@ -0,0 +1,15 @@
+package com.tikoyapps.justgothome;
+
+import android.app.Application;
+
+/**
+ * Created by xcptan on 4/27/16.
+ */
+public class JGHApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ app/src/main/java/com/tikoyapps/justgothome/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tikoyapps.justgothome;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xcptan on 4/27/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(
+        @Nullable
+        Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
